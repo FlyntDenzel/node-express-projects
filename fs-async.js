7b8ad6927c786f3ef5dd9fa50b8eb3a1a711065c
FlyntDenzel <flyntdenzel2004@gmail.com>
2025-12-08 13:43:54 +0100

    async built in module

--- fs-async.js
+++ fs-async.js
@@ -1,5 +1,6 @@
 const { readFile, writeFile } = require("fs");
 
+console.log('start task')
 readFile("./content/first.txt", "utf8", (err, result) => {
   if (err) {
     console.log(err);
@@ -19,7 +20,8 @@ readFile("./content/first.txt", "utf8", (err, result) => {
             console.log(err)
             return
         }
-        console.log(result)
+        console.log('done with this task')
     });
   });
 });
+console.log('start of a new task')
